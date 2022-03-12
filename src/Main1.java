2fdc69a178b5212d36c0ae12aeb6040efb10d13c
kacper.mackowiak <kacpermackowiak256@gmail.com>
2022-03-12 16:08:53 +0100

    sada

---
+++ src/Main1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main1 {
+    public static void main(String[] args){
+
+        File file = new File("file.txt");
+        try { //try-with-resources == using()
+            FileInputStream stream = new FileInputStream(file);
+            InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+
+            char[] buffer = new char [256];
+            StringBuilder builder = new StringBuilder();
+            while (true) {
+                int readBytes = reader.read(buffer,0, buffer.length);
+                if (readBytes == -1) {
+                    break;
+                }
+                builder.append(buffer, 0, readBytes);
+            }
+            String text = builder.toString();
+            System.out.println(text);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+}
